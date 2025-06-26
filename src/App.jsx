bf62e863f6e5bc010bbb9e174cf2962ee2d9e7d4
EngAhmedAbdullaliAli <Ahmedatoore2024@gmail.com>
2025-06-26 12:11:56 -0400

    Wew

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-      <h1 className='text-4xl text-red-800 font-bold text-center'>Welcome to course ms</h1>
+      <div>
+        <h1 className='text-4xl text-red-800 font-bold text-center'>Welcome to course ms</h1>
+        <h2>Welcome Engineers</h2>
+      </div>
     </>
   )
 }
