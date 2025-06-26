b7e2491eb89e980099a26c566156fe1ab86ea06e
Mohamud <maxamuud632@gmail.com>
2025-06-26 11:42:43 +0300

    Initialized react app with vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-4xl text-red-800 font-bold text-center'>Welcome to course ms</h1>
+    </>
+  )
+}
+
+export default App
